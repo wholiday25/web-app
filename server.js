fb19c297617334f34b473e2e0a80b8b509360a6c
akhlaq1 <akhlaq.computer@gmail.com>
2019-08-07 06:01:42 +0500

    v2

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const path=require('path')
 
 
 app.use('/public',express.static(__dirname+'/public'));
+app.use('/login',express.static(__dirname+'/login.html'));
 
 app.listen( 8080);
 
-app.get('/*',function(req,res){
+app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/login.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/login.html'));
+});
+
+app.get('/register.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/register.html'));
 });
 
 console.log("Express server working")
